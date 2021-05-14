89a57f36146f1eb1c0d174c369ba38db8f1023bf
Kaleb Berry <kaleb.berry@outlook.com>
2021-05-14 03:27:28 -0500

    added npm and parcel

--- js/main.js
+++
@@ -1,4 +0,0 @@
-var svg = document.getElementsByTagName("svg")[0];
-var bbox = svg.getBBox();
-var viewBox = [bbox.x, bbox.y, bbox.width, bbox.height].join(" ");
-svg.setAttribute("viewBox", viewBox);
